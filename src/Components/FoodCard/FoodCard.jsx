fa1d9bf2511ed6a9163f1eecb232fb689ff5e55c
Rasibul <rasibul179@gmail.com>
2023-12-15 12:01:17 +0600

    create a popular food item by react slick caursol

---
+++ src/Components/FoodCard/FoodCard.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
+
+
+const FoodCard = ({ item }) => {
+    const [isHeartFiled, setIsHeartFilled] = useState(false)
+    const handelHeartClick = () => {
+        setIsHeartFilled(!isHeartFiled)
+    }
+    return (
+        <div className="card md:w-96 bg-base-100 shadow-xl relative">
+            <div onClick={handelHeartClick} className={`rating gap-1 absolute right-2 top-2 p-4 bg-green heartStar ${isHeartFiled ? "text-rose-500" : "text-white"}`}>
+                <FaHeart className="h-5 w-5 cursor-pointer"></FaHeart>
+            </div>
+            <Link to={`/menu/${item._id}`}>
+                <figure>
+                    <img
+                        src={item?.image}
+                        alt="food img"
+                        className="hover:scale-105 transition-all duration-200  md:h-72"
+                    />
+                </figure>
+            </Link>
+            <div className="card-body p-4">
+                <h2 className="card-title text-lg md:text-xl">{item?.name}</h2>
+                <p className="hidden md:block">{item?.recipe}</p>
+                <div className="card-actions flex justify-between items-center mt-2">
+                    <h5 className="font-semibold"><span className="text-sm text-red">$</span>{item.price}</h5>
+                    <button className="btn bg-green text-white">Buy Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "./Banner";
 import Categories from "./Categories";
+import SpecialDishes from "./SpecialDishes";
 
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Categories></Categories>
+            <SpecialDishes></SpecialDishes>
         </div>
     );
 };

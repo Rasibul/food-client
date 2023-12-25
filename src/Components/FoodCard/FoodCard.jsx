2d99d6472fbef6bce44bf3bc5e51089a21c6b463
Rasibul <rasibul179@gmail.com>
2023-12-26 01:23:12 +0600

    food add to cart page

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,13 +1,41 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa";
+import useAuth from "../../Hooks/useAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 
 const FoodCard = ({ item }) => {
+    const {name,recipe,image,price,_id} = item
     const [isHeartFiled, setIsHeartFilled] = useState(false)
+    const {user} = useAuth()
     const handelHeartClick = () => {
         setIsHeartFilled(!isHeartFiled)
     }
+
+    const handelAddToCArt = async (item) =>{
+        // console.log(item)
+        if(user && user?.email){
+            const cartItem = {
+                menuItemId:_id,
+                price,image,recipe,name,
+                email:user.email
+            }
+            // console.log(cartItem);
+
+            const allCart = await axios.post('http://localhost:5000/carts',cartItem)
+            // console.log(allCart)
+            if(allCart.data.insertedId){
+                Swal.fire({
+                    title: `${name} Added Successfully`,
+                    text: "You clicked the button!",
+                    icon: "success"
+                  });
+            }
+        }
+    }
+
     return (
         <div className="card md:w-96 bg-base-100 shadow-xl relative">
             <div onClick={handelHeartClick} className={`rating gap-1 absolute right-2 top-2 p-4 bg-green heartStar ${isHeartFiled ? "text-rose-500" : "text-white"}`}>
@@ -27,7 +55,7 @@ const FoodCard = ({ item }) => {
                 <p className="hidden md:block">{item?.recipe}</p>
                 <div className="card-actions flex justify-between items-center mt-2">
                     <h5 className="font-semibold">Price: <span className="text-sm text-red">$</span>{item.price}</h5>
-                    <button className="btn bg-green text-white">Buy Now</button>
+                    <button onClick={()=>handelAddToCArt(item)} className="btn bg-green text-white">Add To Cart</button>
                 </div>
             </div>
         </div>
--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -2,9 +2,66 @@
 
 const CartPage = () => {
     return (
-        <div>
-            
+        <div className="section-container">
+            <div className="xl:px-24 bg-gradient-to-r from-0% from-[#FAFAFA] to-[#FCFCFC] to-100%">
+                <div className="py-28 flex items-center justify-center">
+                    {/* texts */}
+                    <div className="px-8 text-center"> {/* Added text-center class */}
+                        <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+                            Items Added To The <span className="text-green">Cart</span>
+                        </h2>
+                    </div>
+                </div>
+            </div>
+            {/* tabel cart */}
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead className="bg-green text-white">
+                            <tr>
+                                <th>#</th>
+                                <th>Food</th>
+                                <th>Item Name</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            <tr>
+                                <th>1</th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">Hart Hagerty</div>
+                                            <div className="text-sm opacity-50">United States</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Zemlak, Daniel and Leannon
+                                    <br />
+                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                </td>
+                                <td>Purple</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
+
+
     );
 };
 
